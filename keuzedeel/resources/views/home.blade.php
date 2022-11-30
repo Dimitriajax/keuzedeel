e11da20d598eb4370901f4194c6ba237388a039b
Dimitriajax <dimitri.brontsema@gmail.com>
2022-11-30 15:48:46 +0100

    Setting up Laravel

---
+++ keuzedeel/resources/views/home.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Home</title>
+</head>
+
+<body>
+    <x-app-layout>
+        <x-slot name="header">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                {{ __('Home') }}
+            </h2>
+        </x-slot>
+    </x-app-layout>
+</body>
+
+</html>
