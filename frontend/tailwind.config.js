002aa16cc6e3b98bec2b30b884b3c1f5ce479d51
timo zuidinga <tzuidinga@st.noorderpoort.nl>
2023-01-19 11:41:30 +0100

    maded home page

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,8 +3,11 @@ module.exports = {
   content: ["**.html"],
   theme: {
     extend: {
-      colors:{
+      colors: {
         "bit-gray": "#F8F9FA",
+        "light-blue": "#40eeee",
+        "rose": "#e2539f",
+        "yelow":"#e8d155"
       }
     },
   },
