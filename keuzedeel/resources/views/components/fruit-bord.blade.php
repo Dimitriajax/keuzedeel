4acf7bacae2b59e72da14dc974469bb1ce386997
Lotte De vries <ldevries38@st.noorderpoort.nl>
2023-01-24 15:01:54 +0100

    50% responsive

---
+++ keuzedeel/resources/views/components/fruit-bord.blade.php
@@ -0,0 +1,46 @@
+<svg id="Component_6_1" height="200" data-name="Component 6 – 1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 462 298">
+    <defs>
+      <radialGradient id="radial-gradient" cx="0.5" cy="0.5" r="0.377" gradientUnits="objectBoundingBox">
+        <stop offset="0" stop-color="#cceaf7"/>
+        <stop offset="1" stop-color="#fdffc3"/>
+      </radialGradient>
+    </defs>
+    <rect id="Rectangle_61" data-name="Rectangle 61" width="140" height="30" transform="translate(123) rotate(90)" fill="#fff"/>
+    <rect id="Rectangle_62" data-name="Rectangle 62" width="140" height="30" transform="translate(347) rotate(90)" fill="#fff"/>
+    <rect id="Rectangle_63" data-name="Rectangle 63" width="440" height="151" transform="translate(11 136)" fill="#fff"/>
+    <text id="_" data-name="🍉" transform="translate(143 246)" font-size="100" font-family="AppleColorEmoji, Apple Color Emoji"><tspan x="-100" y="0">🍉</tspan></text>
+    <text id="_2" data-name="🍎" transform="translate(419 246)" font-size="100" font-family="AppleColorEmoji, Apple Color Emoji"><tspan x="-100" y="0">🍎</tspan></text>
+    <text id="_3" data-name="🍋" transform="translate(281 251)" font-size="100" font-family="AppleColorEmoji, Apple Color Emoji"><tspan x="-100" y="0">🍋</tspan></text>
+    <ellipse id="Ellipse_99" data-name="Ellipse 99" cx="11" cy="10.5" rx="11" ry="10.5" transform="translate(440 277)" fill="url(#radial-gradient)"/>
+    <ellipse id="Ellipse_100" data-name="Ellipse 100" cx="11" cy="10.5" rx="11" ry="10.5" transform="translate(440 238)" fill="url(#radial-gradient)"/>
+    <ellipse id="Ellipse_101" data-name="Ellipse 101" cx="11" cy="10.5" rx="11" ry="10.5" transform="translate(440 126)" fill="url(#radial-gradient)"/>
+    <ellipse id="Ellipse_102" data-name="Ellipse 102" cx="11" cy="10.5" rx="11" ry="10.5" transform="translate(440 199)" fill="url(#radial-gradient)"/>
+    <ellipse id="Ellipse_103" data-name="Ellipse 103" cx="11" cy="10.5" rx="11" ry="10.5" transform="translate(440 163)" fill="url(#radial-gradient)"/>
+    <ellipse id="Ellipse_104" data-name="Ellipse 104" cx="11" cy="10.5" rx="11" ry="10.5" transform="translate(0 126)" fill="url(#radial-gradient)"/>
+    <ellipse id="Ellipse_105" data-name="Ellipse 105" cx="11" cy="10.5" rx="11" ry="10.5" transform="translate(0 277)" fill="url(#radial-gradient)"/>
+    <ellipse id="Ellipse_106" data-name="Ellipse 106" cx="11" cy="10.5" rx="11" ry="10.5" transform="translate(0 238)" fill="url(#radial-gradient)"/>
+    <ellipse id="Ellipse_107" data-name="Ellipse 107" cx="11" cy="10.5" rx="11" ry="10.5" transform="translate(0 199)" fill="url(#radial-gradient)"/>
+    <ellipse id="Ellipse_108" data-name="Ellipse 108" cx="11" cy="10.5" rx="11" ry="10.5" transform="translate(0 163)" fill="url(#radial-gradient)"/>
+    <ellipse id="Ellipse_109" data-name="Ellipse 109" cx="11" cy="10.5" rx="11" ry="10.5" transform="translate(40 277)" fill="url(#radial-gradient)"/>
+    <ellipse id="Ellipse_110" data-name="Ellipse 110" cx="11" cy="10.5" rx="11" ry="10.5" transform="translate(80 277)" fill="url(#radial-gradient)"/>
+    <ellipse id="Ellipse_111" data-name="Ellipse 111" cx="11" cy="10.5" rx="11" ry="10.5" transform="translate(120 277)" fill="url(#radial-gradient)"/>
+    <ellipse id="Ellipse_112" data-name="Ellipse 112" cx="11" cy="10.5" rx="11" ry="10.5" transform="translate(157 277)" fill="url(#radial-gradient)"/>
+    <ellipse id="Ellipse_113" data-name="Ellipse 113" cx="11" cy="10.5" rx="11" ry="10.5" transform="translate(160 277)" fill="url(#radial-gradient)"/>
+    <ellipse id="Ellipse_114" data-name="Ellipse 114" cx="11" cy="10.5" rx="11" ry="10.5" transform="translate(200 277)" fill="url(#radial-gradient)"/>
+    <ellipse id="Ellipse_115" data-name="Ellipse 115" cx="11" cy="10.5" rx="11" ry="10.5" transform="translate(240 277)" fill="url(#radial-gradient)"/>
+    <ellipse id="Ellipse_116" data-name="Ellipse 116" cx="11" cy="10.5" rx="11" ry="10.5" transform="translate(280 277)" fill="url(#radial-gradient)"/>
+    <ellipse id="Ellipse_117" data-name="Ellipse 117" cx="11" cy="10.5" rx="11" ry="10.5" transform="translate(320 277)" fill="url(#radial-gradient)"/>
+    <ellipse id="Ellipse_118" data-name="Ellipse 118" cx="11" cy="10.5" rx="11" ry="10.5" transform="translate(360 277)" fill="url(#radial-gradient)"/>
+    <ellipse id="Ellipse_119" data-name="Ellipse 119" cx="11" cy="10.5" rx="11" ry="10.5" transform="translate(400 277)" fill="url(#radial-gradient)"/>
+    <ellipse id="Ellipse_120" data-name="Ellipse 120" cx="11" cy="10.5" rx="11" ry="10.5" transform="translate(40 125)" fill="url(#radial-gradient)"/>
+    <ellipse id="Ellipse_121" data-name="Ellipse 121" cx="11" cy="10.5" rx="11" ry="10.5" transform="translate(80 126)" fill="url(#radial-gradient)"/>
+    <ellipse id="Ellipse_122" data-name="Ellipse 122" cx="11" cy="10.5" rx="11" ry="10.5" transform="translate(120 126)" fill="url(#radial-gradient)"/>
+    <ellipse id="Ellipse_123" data-name="Ellipse 123" cx="11" cy="10.5" rx="11" ry="10.5" transform="translate(160 126)" fill="url(#radial-gradient)"/>
+    <ellipse id="Ellipse_124" data-name="Ellipse 124" cx="11" cy="10.5" rx="11" ry="10.5" transform="translate(200 126)" fill="url(#radial-gradient)"/>
+    <ellipse id="Ellipse_125" data-name="Ellipse 125" cx="11" cy="10.5" rx="11" ry="10.5" transform="translate(240 126)" fill="url(#radial-gradient)"/>
+    <ellipse id="Ellipse_126" data-name="Ellipse 126" cx="11" cy="10.5" rx="11" ry="10.5" transform="translate(280 126)" fill="url(#radial-gradient)"/>
+    <ellipse id="Ellipse_127" data-name="Ellipse 127" cx="11" cy="10.5" rx="11" ry="10.5" transform="translate(320 126)" fill="url(#radial-gradient)"/>
+    <ellipse id="Ellipse_128" data-name="Ellipse 128" cx="11" cy="10.5" rx="11" ry="10.5" transform="translate(360 125)" fill="url(#radial-gradient)"/>
+    <ellipse id="Ellipse_129" data-name="Ellipse 129" cx="11" cy="10.5" rx="11" ry="10.5" transform="translate(400 125)" fill="url(#radial-gradient)"/>
+  </svg>
+  
